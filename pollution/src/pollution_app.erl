05028cdd8e352bf49580f44456f494b2031708c1
Karol Szuster <kszuster@protonmail.com>
2019-04-13 17:41:59 +0200

    finishing touches

--- pollution/src/pollution_app.erl
+++ pollution/src/pollution_app.erl
@@ -8,20 +8,14 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, start/0, stop/1, stop/0]).
+-export([start/2, stop/1]).
 
 %%====================================================================
 %% API
 %%====================================================================
 
-start() ->
-    application:start(?MODULE).
-
 start(_StartType, _StartArgs) ->
     pollution_sup:start_link().
-
-stop() ->
-    application:stop(?MODULE).
 
 stop(_State) ->
     ok.
